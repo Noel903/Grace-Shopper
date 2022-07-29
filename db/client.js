6825f57aaa920eb99ba30e4de7f26b80a4e9d409
Noel903 <noelmoreno445@gmail.com>
2022-07-28 20:15:38 -0500

    nothing really

--- db/client.js
+++ db/client.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 const DB_NAME = 'capstone'
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`,
+    connectionString: process.env.DATABASE_URL || `postgres://localhost:3001/${ DB_NAME }`,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
   });
 
